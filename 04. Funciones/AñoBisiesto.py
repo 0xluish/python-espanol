67c1349ef1d18a863e69f49b81977729e7885908
0xluish <luishsolis@pm.me>
2022-11-06 19:58:40 -0600

    First commit

--- 04. Funciones/AñoBisiesto.py
+++ 04. Funciones/AñoBisiesto.py
@@ -1,7 +1,10 @@
 año = int(input("\nIngrese el año del que desea saber si es bisiesto o no: "))
 def añoBisiesto(año):
-    if(año%4 != 0 or año%400 != 0):
-        print(f"El año {año} no es bisiesto\n")
-    elif(año%4 == 0 and año%100 == 0 and año%400 == 0):
-        print(f"El año {año} sí es bisiesto\n")
+    if(año%4 == 0):
+        if(año%100 != 0 or año%400 == 0):
+            print(f'El año {año} sí es bisiesto')
+        else:
+            print(f'El año {año} no es bisiesto')          
+    else:   
+        print(f'El año {año} no es bisiesto')
 añoBisiesto(año)
